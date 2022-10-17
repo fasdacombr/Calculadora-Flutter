e72b54bcdd9f56e0b2a3f705953a96395a197475
Flavio <flavio_125@hotmail.com>
2022-10-17 07:20:29 -0300

    Inicio do versionamento

---
+++ lib/components/keyboard.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Keyboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500,
+      child: Column(
+        children: <Widget>[
+          ElevatedButton(
+            child: Text('9'),
+            onPressed: () {},
+          ),
+          ElevatedButton(
+            child: Text('9'),
+            onPressed: () {},
+          ),
+          ElevatedButton(
+            child: Text('9'),
+            onPressed: () {},
+          ),
+        ]
+      ),
+    );
+  }
+  
+  
+}
