40f9234e194e350ab5aeb45fd113512e6a20585a
Flavio <flavio_125@hotmail.com>
2022-10-18 21:02:20 -0300

    Finalização do projeto

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -3,11 +3,17 @@ class Memory {
 
   final _buffer = [0.0, 0.0];
   int _bufferIndex = 0;
-  String operation = '';
+  String? _operation;
   String _value = '0';
   bool _wipeValue = false;
+  String? _lastCommand;
 
   void applyCommand(String command) {
+    if(_isReplacingOperation(command)) {
+      _operation = command;
+      return;
+    }
+
     if(command == 'AC') {
       _allClear();
     } else if(operations.contains(command)) {
@@ -15,10 +21,37 @@ class Memory {
     } else {
       _addDigit(command);  
     }
+
+    _lastCommand = command;
+  }
+
+  _isReplacingOperation(String command) {
+    return operations.contains(_lastCommand)
+      && operations.contains(command)
+      && _lastCommand != '='
+      && command != '=';
   }
 
   _setOperation(String newOperation) {
-    _wipeValue = true;
+    bool isEqualSign = newOperation == '=';
+    if(_bufferIndex == 0) {
+      if(_bufferIndex == 0) {
+        _operation = newOperation;
+        _bufferIndex = 1;
+        _wipeValue = true;
+      }
+    } else {
+      _buffer[0] = _calculate();
+      _buffer[1] = 0.0;
+      _value = _buffer[0].toString();
+      _value = _value.endsWith('.0') ? _value.split('.') [0] : _value;
+
+      
+      _operation = isEqualSign ? null : newOperation;
+      _bufferIndex = isEqualSign ? 0 : 1; 
+    }
+
+    _wipeValue = true; //!isEqualSign;
   }
 
   _addDigit(String digit) {
@@ -39,6 +72,21 @@ class Memory {
 
   _allClear() {
     _value = '0';
+    _buffer.setAll(0, [0.0, 0.0]);
+    _bufferIndex = 0;
+    _operation = null;
+    _wipeValue = false;
+  }
+
+  _calculate() {
+    switch(_operation) {
+      case '%': return _buffer[0] % _buffer[1];
+      case '/': return _buffer[0] / _buffer[1];
+      case 'x': return _buffer[0] * _buffer[1];
+      case '-': return _buffer[0] - _buffer[1];
+      case '+': return _buffer[0] + _buffer[1];
+      default: return _buffer[0];
+    }
   }
 
   String get value {
